bd03ec62833ce920894f3395f31be6f7337e805a
avk2996 <avenkatakrishna25@gmail.com>
2020-01-07 22:15:12 +0530

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,3 +1,14 @@
 #checking whether thge number is prime or not
 def isPrime(n):
+	if n<0: 
+		print('the number is negative. please input positive number')
+	if n>0:
+		if n == 2:
+			return True
+		else:
+			for i in range(2,n):
+				if n%i == 0:
+					return False
+				else:
+					return False
 	
